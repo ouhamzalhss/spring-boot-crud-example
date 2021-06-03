cd19baed71d3bd15a4a996ec9759603533b12687
ouhamzalhss <ouhamza.web.pro@gmail.com>
2021-06-03 16:18:15 +0100

    add spring security

--- src/main/java/com/ouhamza/crup_app/dao/StudentRepo.java
+++ src/main/java/com/ouhamza/crup_app/dao/StudentRepo.java
@@ -6,12 +6,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 /**
  * @author Lhouceine OUHAMZA
  */
 @Repository
 public interface StudentRepo extends JpaRepository<Student, Long> {
-    Student findByNameContains(String name);
+
+    Page<Student> findByNameContains(String name, Pageable pageable);
 }
